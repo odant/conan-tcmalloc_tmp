440fae63f73498639a605492c2b54592a2fc6b5c
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2019-03-13 22:36:48 +0300

    First build

---
+++ test_package/conanfile.py
@@ -0,0 +1,23 @@
+from conans import ConanFile, CMake
+
+
+class PackageTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self, msbuild_verbosity="normal")
+        cmake.verbose = True
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.pdb", dst="bin", src="bin")
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.so*", dst="bin", src="lib")
+
+    def test(self):
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            self.run("ctest --verbose --build-config %s" % self.settings.build_type)
+        else:
+            self.run("ctest --verbose")
